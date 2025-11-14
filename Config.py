cf0940b3de21693f760d205b4b07025735ca44cf
t <a@a.a>
2025-11-14 16:18:28 +0800

    feat: update

--- Config.py
+++ Config.py
@@ -1,18 +1,14 @@
 from Utils import M10Lidar, PlotLidar
 
 
-class ConfigLidar:
-    def init(self):
-        pass
-
-
 m10Lidar = M10Lidar()
 plotLidar = PlotLidar()
 m10Lidar.connect()
 plotLidar.init()
 
+boundary_points = ([-6000, 1000], [0, 1000], [0, 0], [-6000, 0], [-6000, 1000])
+
 while True:
     m10Lidar.listen()
     plotLidar.update_cloud_points(m10Lidar.xs, m10Lidar.ys)
-    boundary_points = ([-6000, 1000], [0, 1000], [0, 0], [-6000, 0], [-6000, 1000])
     plotLidar.plot_boundary(boundary_points)

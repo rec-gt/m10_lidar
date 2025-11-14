56e0ce6c222e95c2770a20571e67a941dac3eded
t <a@a.a>
2025-11-14 17:13:10 +0800

    feat: update

--- Calibrate.py
+++ Calibrate.py
@@ -1,15 +1,21 @@
-from Utils import M10Lidar, PlotLidar, ConfigSystem
+from Utils import M10Lidar, PlotLidar, ConfigSystem, Counter
 
 configSystem = ConfigSystem()
 m10Lidar = M10Lidar()
 plotLidar = PlotLidar()
+counter = Counter(100)
 
 configSystem.read()
 m10Lidar.connect()
 plotLidar.init()
 
-while True:
-    m10Lidar.listen()
+
+def cb():
     plotLidar.update_cloud_points(m10Lidar.xs, m10Lidar.ys)
     plotLidar.plot_boundary(configSystem.boundary_points)
     plotLidar.plot_calibration_point(configSystem.calibration_point)
+
+
+while True:
+    m10Lidar.listen()
+    counter.auto_counter(cb)

f60da3b61a8c27c022c6cb52fec166fb208520bf
t <a@a.a>
2025-11-15 10:12:29 +0800

    feat: update

--- test.py
+++ test.py
@@ -1,24 +1,38 @@
+import asyncio
+
 from pymodbus import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock, ModbusDeviceContext, ModbusServerContext
-from pymodbus.server import StartSerialServer
+from pymodbus.server import StartAsyncSerialServer
 
-store = ModbusDeviceContext(
-    hr=ModbusSequentialDataBlock(0, [17] * 100),
-)
-context = ModbusServerContext(devices=store, single=True)
 identity = ModbusDeviceIdentification()
 identity.VendorName = 'RGT'
 identity.ProductCode = 'RGT-LIDAR'
 identity.ProductName = 'RGT-LIDAR'
 
-print("Starting Modbus RTU Server on COM port...")
-StartSerialServer(
-    context=context,
-    identity=identity,
-    port='COM22',
-    baudrate=9600,
-    bytesize=8,
-    parity='N',
-    stopbits=1,
-    timeout=1,
-)
+
+async def start():
+    store = ModbusDeviceContext(
+        hr=ModbusSequentialDataBlock(0, [17] * 100),
+    )
+    context = ModbusServerContext(devices=store, single=True)
+    print("Starting Modbus RTU Server on COM port...")
+    server = await StartAsyncSerialServer(
+        context=context,
+        identity=identity,
+        port='COM22',
+        baudrate=9600,
+        bytesize=8,
+        parity='N',
+        stopbits=1,
+        timeout=1,
+    )
+
+    try:
+        await asyncio.Event().wait()  # runs forever (Ctrl+C to stop)
+    except KeyboardInterrupt:
+        print("\nStopping server…")
+    finally:
+        await server.shutdown()  # clean shutdown
+
+
+asyncio.run(start())

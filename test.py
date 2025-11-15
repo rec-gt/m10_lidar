3b1e4e9486bb9698f390cee9c4040a51f896a8dc
t <a@a.a>
2025-11-15 10:01:02 +0800

    feat: update

--- test.py
+++ test.py
@@ -7,15 +7,15 @@ store = ModbusDeviceContext(
 )
 context = ModbusServerContext(devices=store, single=True)
 identity = ModbusDeviceIdentification()
-identity.VendorName = 'MyCompany'
-identity.ProductCode = 'MC'
-identity.ProductName = 'Test RTU Server'
+identity.VendorName = 'RGT'
+identity.ProductCode = 'RGT-LIDAR'
+identity.ProductName = 'RGT-LIDAR'
 
 print("Starting Modbus RTU Server on COM port...")
 StartSerialServer(
     context=context,
     identity=identity,
-    port='COM3',  # Change to your port: 'COM3', '/dev/ttyUSB0', etc.
+    port='COM22',
     baudrate=9600,
     bytesize=8,
     parity='N',

d90f5f94f9190381e1ce19d1544f6a0610f55627
t <a@a.a>
2025-11-21 11:34:06 +0800

    feat: update

--- Main.py
+++ Main.py
@@ -59,14 +59,12 @@ class OSConfig:
 
 class SystemConfig:
     boundary_points = (())
-    calibration_point = ()
     polygon_points = (())
     boundary_profile = "DEFAULT"
     boundary = (())
 
     def __init__(self):
         self.boundary_points = (())
-        self.calibration_point = ()
         self.polygon_points = (())
         self.boundary_profile = "DEFAULT"
         self.boundary = (())
@@ -78,17 +76,15 @@ class SystemConfig:
             (CONFIG_POINTS["BOTTOM_RIGHT_POINT"]["x"], CONFIG_POINTS["BOTTOM_RIGHT_POINT"]["y"]),
             (CONFIG_POINTS["BOTTOM_LEFT_POINT"]["x"], CONFIG_POINTS["BOTTOM_LEFT_POINT"]["y"]),
             (CONFIG_POINTS["TOP_LEFT_POINT"]["x"], CONFIG_POINTS["TOP_LEFT_POINT"]["y"]),
+            (CONFIG_POINTS["CALIBRATION_POINT"]["x"], CONFIG_POINTS["CALIBRATION_POINT"]["y"])
         )
-
-        self.calibration_point = (
-            CONFIG_POINTS["CALIBRATION_POINT"]["x"], CONFIG_POINTS["CALIBRATION_POINT"]["y"])
 
         self.polygon_points = CONFIG_POLYGON
 
         self.boundary_profile = CONFIG_SYSTEM["BOUNDARY_PROFILE"]
 
         if self.boundary_profile == "DEFAULT":
-            self.boundary = list(self.boundary_points).append(self.calibration_point)
+            self.boundary = self.boundary_points
         else:
             self.boundary = self.polygon_points
 
